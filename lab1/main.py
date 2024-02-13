d71ce39f79bb44c737891491893560980b5ad34f
Madina Baltayeva <101057294+madinabltv@users.noreply.github.com>
2024-02-13 15:25:03 +0300

    Update main.py

--- lab1/main.py
+++ lab1/main.py
@@ -3,9 +3,29 @@ A = [[2, -1, 0],
      [0, 1, -3]]
 
 d = [3, 6, 2]
+# необходимые и достаточные условия
+def necessity(A):
+    n = len(A)
+    for i in range(1, n-1):
+        if A[i][i] < A[i][i+1]:
+            print('Не выполнено условия диагонального преобладания')
+            return False
+
+    for i in range(1, n-1):
+        if A[i][i] == 0:
+            print('Нулевые элементы на главной диагонали')
+            return False
+    return True
+
+
 
 def find_x(A, d):
     n = len(A)
+
+    if (not necessity(A)):
+        print("Условия необходимости и достаточности не выполняются")
+        return -1
+
     x = [0 for k in range(0, n)]
 
     alpha = [0 for k in range(0, n)]
@@ -17,9 +37,9 @@ def find_x(A, d):
     beta[0] = d[0] / Y[0]
 
     for i in range(1,n-1):
-        Y[i] = A[i][i] + A[i][i-1] * alpha[i-1]
+        Y[i] = A[i][i] + A[i][i-1]*alpha[i-1]
         alpha[i] = - A[i][i+1] / Y[i]
-        beta[i] = (d[i]-A[i][i-1] * beta[i-1]) / Y[i]
+        beta[i] = (d[i]-A[i][i-1]*beta[i-1]) / Y[i]
 
     Y[n-1] = A[n-1][n-1] + A[n-1][n-2] * alpha[n-2]
     beta[n-1] = (d[n-1] - A[n-1][n-2] * beta[n-2]) / Y[n-1]
