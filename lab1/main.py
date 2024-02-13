01d7053c810abd8643ecf37bf97f0b821b9ffce7
Madina Baltayeva <101057294+madinabltv@users.noreply.github.com>
2024-02-13 22:03:57 +0300

    Update main.py

--- lab1/main.py
+++ lab1/main.py
@@ -1,13 +1,26 @@
+import numpy as np
+
 A = [[2, -1, 0],
      [5, 4, 2],
      [0, 1, -3]]
 
-d = [3, 6, 2]
+d = [3, 6, 1]
 # необходимые и достаточные условия
 def necessity(A):
     n = len(A)
+
     for i in range(1, n-1):
-        if A[i][i] < A[i][i+1]:
+        if A[i][i] < A[i-1][i]:
+            print('Не выполнено условия диагонального преобладания')
+            return False
+
+    for i in range(1, n-1):
+        if A[i][i] > A[i][i-1]:
+            print('Не выполнено условия диагонального преобладания')
+            return False
+
+    for i in range(1, n-1):
+        if A[i][i-1] < A[i-1][i] + A[i][i]:
             print('Не выполнено условия диагонального преобладания')
             return False
 
@@ -16,8 +29,6 @@ def necessity(A):
             print('Нулевые элементы на главной диагонали')
             return False
     return True
-
-
 
 def find_x(A, d):
     n = len(A)
@@ -36,7 +47,7 @@ def find_x(A, d):
     alpha[0] = - A[0][1] / Y[0]
     beta[0] = d[0] / Y[0]
 
-    for i in range(1,n-1):
+    for i in range(1, n-1):
         Y[i] = A[i][i] + A[i][i-1]*alpha[i-1]
         alpha[i] = - A[i][i+1] / Y[i]
         beta[i] = (d[i]-A[i][i-1]*beta[i-1]) / Y[i]
@@ -51,7 +62,29 @@ def find_x(A, d):
     x[n-1] = beta[n-1]
     for i in range(n-1, 0, -1):
         x[i-1] = alpha[i-1] * x[i] + beta[i-1]
-    print(x)
+
+    for i in range(0, n):
+        print(f'x[{i}] =', x[i])
+    print()
+
+# погрешность
+
+    d1 = np.dot(A, x)
+    # print(d1)
+
+    r = np.subtract(d, d1)
+    e = [0 for k in range(0, n)]
+
+    A_inv = np.linalg.inv(A)
+    # print(A_inv)
+
+    for i in range(0, n):
+        e[i] += A_inv[i][i] * r[i]
+        print("Погрешность {:.20f}".format(e[i]))
+    print()
+
+    for i in range(0, n):
+        c = x[i] - e[i]
+        print(f'Приближенное значение x[{i}] =', c)
 
 find_x(A, d)
-
