4bc4a6b5b09f823244d8d1bcf6560d985932daf6
Madina Baltayeva <101057294+madinabltv@users.noreply.github.com>
2024-02-28 23:45:16 +0300

    Update main.py

--- lab2/main.py
+++ lab2/main.py
@@ -20,10 +20,8 @@ def main():
         yi[i] = (curx * ((curx) ** 0.5) + 16)
         curx += h
         i += 1
-    # print("y: ", yi)
 
     xi = [0 for k in range(0, n+1)]
-
     curx = from_val
     i = 0
 
@@ -31,10 +29,7 @@ def main():
         xi[i] = curx
         curx += h
         i += 1
-    # print('x:', xi)
-
-
-
+        
     a_coef = [0 for k in range(0, n+1)]
     b_coef = [0 for k in range(0, n+1)]
     c_coef = [0 for k in range(0, n+1)]
@@ -55,14 +50,12 @@ def main():
         y = 4 + 1 * alpha[i - 1]
         alpha[i] = -1 / y
         beta[i] = (d_res[i] - 1 * beta[i - 1]) / y
-    # print(beta, alpha)
 
     c_coef[0] = 0
     c_coef[n] = 0
     c_coef[n-1] = beta[n-1]
     for i in range(n-2, 0, -1):
         c_coef[i] = alpha[i] * c_coef[i+1] + beta[i]
-    # print(c_coef)
 
     for i in range(0, n-1):
         a_coef[i] = yi[i]
@@ -73,28 +66,21 @@ def main():
     d_coef[n-1] = - c_coef[n] / (3 * h)
 
     print('x         S(x)      y(x)        |S(x) - y(x)| ')
-    print('                                 между узлами   ')
     for i in range(0, n):
-        spl = spline(i, xi[i], xi, a_coef, b_coef, c_coef, d_coef)
-
-        x = from_val + (i+1/2) * h
-        y = func(from_val + (i+1/2) * h)
-
-        spl1 = spline(i, x, xi, a_coef, b_coef, c_coef, d_coef)
+        y = func(xi[i])
+        spl1 = spline(i, xi[i], xi, a_coef, b_coef, c_coef, d_coef)
         diff = abs(spl1 - y)
+        
         print(f"{xi[i]:-2.5f}  {spl1:-10.5f}  {y:-10.5f} {diff:-10.5f}")
-
+        
     print()
     print('(x[i]+x[i+1])/2   S(x)      y(x)      |S(x) - y(x)| ')
-    print('                                        в узлах ')
+
     for i in range(0, n):
-            spl = spline(i, xi[i], xi, a_coef, b_coef, c_coef, d_coef)
-
-            x = from_val + (i+1/2) * h
-            y = func(x)
-    
-            spl1 = spline(i, x, xi, a_coef, b_coef, c_coef, d_coef)
-            diff2 = abs(spl - yi[i])
-
-            print(f"{x:-2.5f}        {spl:-10.5f} {yi[i]:-10.5f} {diff2:-10.5f}")
+            nel = (xi[i] +xi[i+1]) / 2
+            spl = spline(i, nel, xi, a_coef, b_coef, c_coef, d_coef)
+            y = func(nel)
+            diff2 = abs(spl - y)
+        
+            print(f"{nel:-2.5f}        {spl:-10.5f} {y:-10.5f} {diff2:-10.5f}")
 main()
